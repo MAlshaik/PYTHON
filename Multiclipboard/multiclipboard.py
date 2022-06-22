43a97ec0ec8807d8555a6f37fdc8b439b1a71b0e
N0H3dy <workmail2776@gmail.com>
2022-06-22 21:40:23 +0300

    basic logic

--- Multiclipboard/multiclipboard.py
+++ Multiclipboard/multiclipboard.py
@@ -1,1 +1,24 @@
-print('hey')
+import sys 
+import clipboard
+import json
+
+def save_items(filepath, data):
+    with open(filepath, 'w') as f:
+        json.dump(data, f)
+
+
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+
+    #perhaps use switch instead 
+    if command == 'save':
+        print('save')
+    elif command == 'load':
+        print(load)
+    elif command == 'list':
+        print('list')
+    else:
+        print('Unknown command')
+else:
+    print("Please pass exactly one command.")
