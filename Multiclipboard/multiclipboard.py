e6bf1d205640faac97442d4898a9158235afdab8
N0H3dy <workmail2776@gmail.com>
2022-06-24 15:18:18 +0300

    save and load command done

--- Multiclipboard/multiclipboard.py
+++ Multiclipboard/multiclipboard.py
@@ -1,21 +1,39 @@
+from dataclasses import dataclass
 import sys 
 import clipboard
 import json
+
+SAVED_DATA = "clipboard.json"
 
 def save_items(filepath, data):
     with open(filepath, 'w') as f:
         json.dump(data, f)
 
-
+def load_data(filepath):
+    try:
+        with open(filepath, 'r') as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
 
 if len(sys.argv) == 2:
     command = sys.argv[1]
+    data = load_data(SAVED_DATA)
 
     #perhaps use switch instead 
     if command == 'save':
-        print('save')
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+        save_items(SAVED_DATA, data)
+        print('data saved!')
     elif command == 'load':
-        print(load)
+        key = input("Enter a key: ")
+        if key in data:
+            clipboard.copy(data[key])
+            print("data copied to clipboard.")
+        else:
+            print("key does not exist.")
     elif command == 'list':
         print('list')
     else:
